c3720abb32d4a07f42171d52fdfb795e17311da3
Cloud 9 <anmollangara@gmail.com>
2026-03-09 17:11:00 -0700

    modificaitons

--- palindrome.test.js
+++ palindrome.test.js
@@ -4,12 +4,10 @@ test('madam is a palindrome', () => {
   expect(isPalindrome('madam')).toBe(true);
 });
 
+test('racecar is a palindrome', () => {
+  expect(isPalindrome('racecar')).toBe(true);
+});
+
 test('hello is not a palindrome', () => {
   expect(isPalindrome('hello')).toBe(false);
 });
-test('A man, a plan, a canal, Panama is a palindrome', () => {
-  expect(isPalindrome('A man, a plan, a canal, Panama')).toBe(true);
-});
-test('No lemon, no melon is a palindrome', () => {
-  expect(isPalindrome('No lemon, no melon')).toBe(true);
-}); 
